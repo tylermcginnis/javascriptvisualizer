379c8e56406c960c2b967ef3f695a0925d4c1394
Sachin Philip Mathew <me@imsach.in>
2018-10-10 09:41:49 +0530

    kind of temporary fix

--- src/components/App.js
+++ src/components/App.js
@@ -35,7 +35,7 @@ const Container = styled.div`
 const LeftContainer = styled.div`
   display: flex;
   flex-direction: column;
-  height: 100vh
+  height: 100vh;
   width: 50vw;
   overflow: scroll;
 `
